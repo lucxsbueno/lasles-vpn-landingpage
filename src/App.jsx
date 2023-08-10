5add2565c822289225cfbbeeba31f8a25f1a3e9c
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-08-10 16:13:30 -0300

    feat(setup): Configuração inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="text-3xl text-slate-800">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste deserunt ex
+      debitis voluptas a eius nulla cum blanditiis minima, amet possimus ab
+      atque soluta quia ullam ipsa iure nam sapiente!
+    </div>
+  );
+};
+
+export default App;
